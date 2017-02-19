c815fd03334166c3009ece80a37ef3ebde2fe452
Joshua Lawrence <Jlawrence124@gmail.com>
2017-02-19 15:28:20 -0500

    Fixed file structure
    File structure fixed

--- js/app.js
+++ js/app.js
@@ -7,5 +7,12 @@ var ViewModel = function () {
 };
 
 var View = function () {
-
+	function initMap() {
+		// Constructor creates a new map - only center and zoom are required.
+		map = new google.maps.Map(document.getElementById('#map'), {
+		  center: {lat: 35.2271, lng: 80.8431},
+		  zoom: 13,
+		  mapTypeControl: false
+		});
+	}
 };
